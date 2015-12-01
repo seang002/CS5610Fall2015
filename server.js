acd340ca04ad0a26c037a24c4432b10bcbbada7f
Verena <verena.chung@gmail.com>
2015-11-30 22:11:06 -0800

    Miniscule change

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
         process.env.OPENSHIFT_APP_NAME;
 }
-
 var db = mongoose.connect(connectionString);
 
 // Connection
